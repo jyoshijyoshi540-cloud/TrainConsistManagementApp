2ba49a9e05ae1442f15eb0f98e182bc325234aa8
jyoshijyoshi540-cloud <jyoshijyoshi540@gmail.com>
2026-03-24 08:50:34 +0530

    UC1: Initialize Train and Display Consist

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        List<String> consist = new ArrayList<>();
+        System.out.println("Initial bogie count: " + consist.size());
+    }
 }
+git add .
+git commit -m "UC1: Initialize Train and Display Consist "
+git push origin feature/UC1
